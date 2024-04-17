ee14baeda4b3e19a0640226778c1660614e1cc4f
Egor Shkaroburov <egor.shkaroburov@tutors.es>
2024-04-17 20:16:59 +0300

    polling, comments & requirements

--- TELEGRAM_BOT/TELEGRAM_BOT.py
+++ TELEGRAM_BOT/TELEGRAM_BOT.py
@@ -8,14 +8,13 @@ import schedule
 
 bot = telebot.TeleBot("7085433359:AAETrss2V6vHve7eAQKRi3QgXs9m5DLehJs")
 
-@bot.message_handler(commands=['start', 'back'])    
+
+@bot.message_handler(commands=['start', 'back'])
 def start(message):
     markup = types.InlineKeyboardMarkup()
     btn_news = types.InlineKeyboardButton('Helldivers news', callback_data='delete')
     markup.add(btn_news)
     bot.send_message(message.chat.id, 'Hello soldier!', reply_markup=markup)
-
-
 
 @bot.callback_query_handler(func=lambda callback: True)
 def callback_message(callback):
@@ -29,23 +28,18 @@ def callback_message(callback):
             published_time = datetime.utcfromtimestamp(news_item["published"])
             formatted_time = published_time.strftime("%H:%M:%S")
             bot.send_message(callback.message.chat.id, f'Published: {formatted_time}\n{news_item["message"]}')
-            
 
-
-last_processed_id = 2805  # Инициализация переменной для хранения последнего обработанного ID
+last_processed_id = 2805  # Initializing the variable to store the last processed ID
 
 @bot.message_handler(func=lambda message: message.text.lower() == 'ebash')
 def handle_start(message):
-    # Запускаем выполнение планировщика после получения сообщения "start" от пользователя
-        #schedule.every(30).minutes.do(check_api(message))
-        while True:
-            
-            check_api(message)
-            time.sleep(30)
-
+    # Start scheduler execution after receiving the message "start" from the user
+    while True:
+        check_api(message)
+        time.sleep(30)
 
 def check_api(message):
-    global last_processed_id  # Объявляем переменную как глобальную, чтобы иметь доступ к ней из функции
+    global last_processed_id  # Declare the variable as global to access it from the function
     res = requests.get('https://helldiverstrainingmanual.com/api/v1/war/news')
     data = json.loads(res.text)
     for news_item in data:
@@ -55,9 +49,5 @@ def check_api(message):
             published_time = datetime.utcfromtimestamp(news_item["published"])
             formatted_time = published_time.strftime("%H:%M:%S")
             bot.send_message(message.chat.id, f'Published: {formatted_time}\n{news_item["message"]}')
-            
 
-
-
-
-bot.infinity_polling()
+bot.polling(non_stop=True)
